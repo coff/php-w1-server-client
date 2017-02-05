dd5a4bed4708ddac728361a7039d9e01c3ad9448
Krzysztof Kubanek <kkubanek@pureholidayhomes.com>
2017-02-06 00:32:06 +0100

    examples

--- examples/client_async.php
+++ examples/client_async.php
@@ -1,10 +1,30 @@
 #!/usr/bin/php
 <?php
 
-$w1client = new \OneWire\Client\AsyncW1Client();
+namespace OneWire\Examples;
+
+use OneWire\Client\AsyncW1Client;
+use OneWire\ClientTransport\XmlW1ClientTransport;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+include (__DIR__ . '/../vendor/autoload.php');
+
+$w1client = new AsyncW1Client('tcp://0.0.0.0:8000');
+$w1client->setLogger($logger = new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG, $isDecorated=true, new OutputFormatter())));
+
+$w1client->setTransport(new XmlW1ClientTransport());
+$w1client->setAutoDiscoveryCallback(function($id) use ($logger) {
+    $logger->info('Callback has been called with id=' . $id);
+    return true;
+});
+$w1client->setAutoDiscovery(true);
 
 while (true) {
     $w1client->update();
-    sleep(1);
+    usleep(1000000);
+
 }
+
 
--- examples/server.php
+++ examples/server.php
@@ -1,10 +1,10 @@
 #!/usr/bin/php
 <?php
 
-namespace OneWire;
+namespace OneWire\Examples;
 
-use Monolog\Formatter\LogglyFormatter;
 use OneWire\Server\W1Server;
+use OneWire\ServerTransport\XmlW1ServerTransport;
 use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\ConsoleOutput;
@@ -12,7 +12,8 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 include (__DIR__ . '/../vendor/autoload.php');
 
 $s = new W1Server('tcp://0.0.0.0:8000');
-$s->setLogger(new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_VERY_VERBOSE, $isDecorated=true, new OutputFormatter())));
+$s->setLogger(new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG, $isDecorated=true, new OutputFormatter())));
+$s->setTransport(new XmlW1ServerTransport());
 $s->init();
 
 $s->loop();
